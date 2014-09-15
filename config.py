112275e6e8fab2df43cc9c0fc5dcef6df07f22ea
Jon Haddad <jon@jonhaddad.com>
2014-09-15 10:55:05 -0700

    starcraft

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ HTTPSERVER_PORT = "9005"
 # ------------------------------------------------------------------------------------
 
 # The list of rooms will should join.  Default is all rooms.
-ROOMS = ['Evangelists', "Community Team", "training", "Bootcamp" ]
+ROOMS = ['Evangelists', "Community Team", "training", "Bootcamp", "starcraft" ]
 
 
 # The room will will talk to if the trigger is a webhook and he isn't told a specific room.
